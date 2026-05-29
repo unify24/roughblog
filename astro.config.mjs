dab087fb87d55900fb97fdb492cb8f248315170d
user <unify-sushi-hunger@duck.com>
2026-05-29 09:50:19 +0530

    added the webdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+  site: "https://unify24.gitlab.io", // or github.io depending on platform
+  base: "/roughblog",
+});
